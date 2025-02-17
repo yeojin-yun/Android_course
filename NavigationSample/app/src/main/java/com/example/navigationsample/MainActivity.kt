81b6859c758013b7872a4a11ebfff824bb17e498
yeojinyun <sail041@naver.com>
2025-02-17 23:02:44 +0900

    day10: 156~159 네비게이트 시작

---
+++ NavigationSample/app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationSampleTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstscreen") {
+        composable("firstscreen") {
+            FirstScreen {
+                navController.navigate("secondscreen")
+            }
+        }
+        composable("secondscreen") {
+            SecondScreen {
+                navController.navigate("thirdscreen")
+            }
+        }
+        composable("thirdscreen") {
+            ThirdScreen {
+                navController.navigate("firstscreen")
+            }
+        }
+    }
+}
---
+++ NavigationSample/app/src/main/java/com/example/navigationsample/ThirdScreen.kt
@@ -0,0 +1,26 @@
+package com.example.navigationsample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ThirdScreen(navToFirstScreen: ()->Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("This is Third Screen")
+        Button(onClick = {
+            navToFirstScreen()
+        }) {
+            Text("Go To First Screen")
+        }
+    }
+}
