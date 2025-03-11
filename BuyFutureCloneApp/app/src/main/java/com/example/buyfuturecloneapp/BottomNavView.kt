a71735a7583b41638125d20de77a0cbb00207176
yeojinyun <sail041@naver.com>
2025-03-11 23:35:56 +0900

    add clone project

---
+++ BuyFutureCloneApp/app/src/main/java/com/example/buyfuturecloneapp/BottomNavView.kt
@@ -0,0 +1,12 @@
+package com.example.buyfuturecloneapp
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material3.NavigationBar
+import androidx.compose.runtime.Composable
+
+
+
+@Composable
+fun BottomNavView() {
+    NavigationBar()
+}
