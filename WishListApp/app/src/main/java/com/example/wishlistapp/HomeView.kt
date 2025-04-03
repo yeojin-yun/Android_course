23e20f8c44dd15c5c1f5480e50edad64e6bf2c9c
admin <admin@adminui-MacBookPro.local>
2025-04-03 09:11:15 +0900

    day 14 swipeToDismiss completed

--- WishListApp/app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ WishListApp/app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -1,11 +1,15 @@
 package com.example.wishlistapp
 
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissState
 import androidx.compose.material.DismissValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
@@ -15,10 +19,14 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
@@ -64,7 +72,19 @@ fun HomeView(
 
                 SwipeToDismiss(
                     state = dismissState,
-                    background = {},
+                    background = {
+                        val color by animateColorAsState(
+                            if (dismissState.dismissDirection == DismissDirection.EndToStart) Color.Red else Color.Transparent,
+                            label = ""
+                        )
+                        
+                        Box(
+                            modifier = Modifier.fillMaxSize().background(color).padding(horizontal = 20.dp),
+                            contentAlignment = Alignment.CenterEnd
+                        ) {
+                            Icon(Icons.Default.Delete, contentDescription = "Delete Icon", tint = Color.White)
+                        }
+                    },
                     directions = setOf(DismissDirection.EndToStart, DismissDirection.EndToStart),
                     dismissThresholds = {FractionalThreshold(fraction = 0.25f)},
                     dismissContent = {
